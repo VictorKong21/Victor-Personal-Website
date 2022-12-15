cb8ead16ba6b2773ce7392d9329c73ed01ff4f0e
Victor <victorkongym@gmail.com>
2022-12-15 10:24:19 +0000

    reformat code

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // alert("Hi");
 
 // Burger menu collapse when click on links
-$('.navbar-nav a').on('click', function() {
-    $('.navbar-collapse').collapse('hide');
+$(".navbar-nav a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
 });
